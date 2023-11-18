a93c2e5d94dfc6d9a08b2b52ff367e56a6596ccb
simxnet <simxnet@envs.net>
2023-11-18 15:09:12 +0100

    feat: meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,20 @@ import "./globals.css";
 
 import { CookieAlert } from "@/components/cookies";
 import Logo from "@/public/logo.png";
+import type { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: "Zeyr | Discord bot",
+	description:
+		"Zeyr is a powerful and versatile Discord bot designed to enhance yourserver experience.",
+	twitter: {
+		images: ["https://zeyr.vercel.app/logo.png"],
+	},
+	openGraph: {
+		type: "website",
+	},
+};
 
 export const fontSans = FontSans({
 	subsets: ["latin"],
@@ -22,6 +35,7 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
+			<link ref="icon" href="/logo.png" />
 			<ThemeProvider
 				attribute="class"
 				defaultTheme="system"
